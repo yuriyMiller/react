2fc34115d86899759378554c2fcd3c162270cc96
yuriyf <yuriyf@smartinfosys.com>
2018-08-31 18:06:56 +0300

    commit index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-const i = 0;
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const title = React.createElement('h1', null, 'React ToDo'); 
+const subtile = React.createElement('p', {className: 'subTitle'}, 'React First App'); 
+const container = React.createElement('div', null, title, subtile);
+
+const app = (<div>
+                <h1>React  !!!!</h1>
+                <p className='subtitle'>React First App</p>
+            </div>)
+
+function App() {
+    return (<div>
+                <h1>React  !!!!</h1>
+                <p className='subtitle'>React First App</p>
+            </div>)
+}
+
+//ReactDOM.render(app, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+
